5f2e794fdcfd1a8b6e1aaaf3fc3ed29b612d9be0
Akalya <Akalya_Jayaraj@gap.com>
2017-06-22 16:38:11 +0530

    refactored the code

--- src/main/java/com/nisum/FizzBuzzService.java
+++ src/main/java/com/nisum/FizzBuzzService.java
@@ -3,19 +3,17 @@ package com.nisum;
 public class FizzBuzzService {
 
 	public String getResult(int i) {
-		String result = null;
 		if (i%15 ==0){
-			result = "fizzbuzz";
+			return "fizzbuzz";
 		}
 		else if(i % 3 == 0) {
-			result = "fizz";
+			return "fizz";
 		}
 		else if(i % 5 == 0) {
-			result = "buzz";
-		} else{
-			result=String.valueOf(i);
-		}
-		return result;
+			return "buzz";
+		} 
+		return String.valueOf(i);
+		
 	}
 
 }
